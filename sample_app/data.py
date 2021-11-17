f684ff1cee67541fe90f22016be06d200bc2bf9d
Nishita Toshi <74404458+nishitatoshi@users.noreply.github.com>
2021-11-17 23:32:54 +0530

    Articles add, edit, delete, view

---
+++ sample_app/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. At rem necessitatibus labore, quia ut sequi cumque possimus tempore quae provident aperiam reprehenderit dolor quam in fugiat. Incidunt esse molestias placeat?',
+            'author':'Brad',
+            'created_date':'04-25-2017'
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. At rem necessitatibus labore, quia ut sequi cumque possimus tempore quae provident aperiam reprehenderit dolor quam in fugiat. Incidunt esse molestias placeat?',
+            'author':'John',
+            'created_date':'04-25-2017'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. At rem necessitatibus labore, quia ut sequi cumque possimus tempore quae provident aperiam reprehenderit dolor quam in fugiat. Incidunt esse molestias placeat?',
+            'author':'Alex',
+            'created_date':'04-25-2017'
+        }
+    ]
+    return articles
